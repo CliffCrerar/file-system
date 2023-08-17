2ca1f0ce639a2ee8144730af0b9017aaf3aa8a7d
Cliff Crerar <cliff.crerar@gmail.com>
2023-08-17 13:40:32 +0200

    modified webpack build file

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,25 @@ const {
 	NODE_ENV = 'production',
 } = process.env;
 module.exports = {
-	entry: './src/app/index.ts',
+	entry: './src/api/index.ts',
 	mode: NODE_ENV,
 	target: 'node',
 	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'index.js'
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+		clean: true
 	},
 	resolve: {
 		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: [
+					'ts-loader',
+				]
+			}
+		]
 	}
 }
